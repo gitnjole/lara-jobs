870bca7c69b53a6af313161064d3c304ca99907c
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-21 18:18:27 +0100

    style(footer): update footer styling and add github link

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -49,13 +49,16 @@
     @yield('content')
     </main>
     <footer
-    class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
->
-    <p class="ml-2">Copyright &copy; {{date("Y")}}, All Rights reserved</p>
-
+    class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-12 mt-12 opacity-90 md:justify-center">
+    <p class="ml-2">
+        <span style="vertical-align: middle;">{{ date("Y") }},</span>
+        <a href="https://github.com/gitnjole/lara-jobs" target="_blank" style="display: inline-block; vertical-align: middle;">
+            <img src="{{ asset('images/github-mark.png') }}" width="20" height="20" alt="GitHub Mark">
+        </a>
+    </p>      
     <a
         href="/listings/create"
-        class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
+        class="absolute right-10 bg-black text-white py-2 px-5"
         >Post Job</a
     >
 </footer>
