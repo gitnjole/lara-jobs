515c293cc11e7718a35c7e04847a049f3ae6d9bb
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-21 14:17:50 +0100

    feat(old): add old data to failed forms for user accesibility

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -23,6 +23,7 @@ class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24"
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="company_name"
+            value="{{old('company_name')}}"
         />
         @error('company_name')
         <p class="text-red-500">{{$message}}</p>
@@ -38,6 +39,7 @@ class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24"
             class="border border-gray-200 rounded p-2 w-full"
             name="title"
             placeholder="Example: Senior Laravel Developer"
+            value="{{old('title')}}"
         />
         @error('title')
         <p class="text-red-500">{{$message}}</p>
@@ -55,6 +57,7 @@ class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24"
             class="border border-gray-200 rounded p-2 w-full"
             name="location"
             placeholder="Example: Remote, Boston MA, etc"
+            value="{{old('location')}}"
         />
         @error('location')
         <p class="text-red-500">{{$message}}</p>
@@ -69,6 +72,7 @@ class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24"
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="contact_email"
+            value="{{old('contact_email')}}"
         />
         @error('contact_email')
         <p class="text-red-500">{{$message}}</p>
@@ -86,6 +90,7 @@ class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24"
             type="text"
             class="border border-gray-200 rounded p-2 w-full"
             name="website"
+            value="{{old('website')}}"
         />
         @error('website')
         <p class="text-red-500">{{$message}}</p>
@@ -101,6 +106,7 @@ class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24"
             class="border border-gray-200 rounded p-2 w-full"
             name="tags"
             placeholder="Example: Laravel, Backend, Postgres, etc"
+            value="{{old('tags')}}"
         />
         @error('tags')
         <p class="text-red-500">{{$message}}</p>
@@ -130,7 +136,7 @@ class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24"
             name="description"
             rows="10"
             placeholder="Include tasks, requirements, salary, etc"
-        ></textarea>
+        >{{old('description')}}</textarea>
     </div>
 
     <div class="mb-6">
