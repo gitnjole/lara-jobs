188761a1841ad792609d52c1884f7cef2cfee73c
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-21 22:45:01 +0100

    refactor(links): update anchor tags for registration

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -14,11 +14,10 @@ class="relative h-72 bg-laravel flex flex-col justify-center align-center text-c
         Find or post Laravel jobs & projects
     </p>
     <div>
-        <a
-            href="register.html"
-            class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
-            >Sign Up to List a Gig</a
-        >
+        <a 
+        href="/register"
+        class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
+        >Sign Up to List a Job</a>
     </div>
 </div>
 </section>
