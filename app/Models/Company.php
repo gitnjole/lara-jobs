9bb10d7cc6d15abe8ec4a35cc1912d39fe768ed2
Antonio Cahun <cahunantonio1@gmail.com>
2024-04-21 13:05:14 +0200

    feat(Company): Add Company model, migration and database factory

---
+++ app/Models/Company.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'password',
+        'location',
+        'email',
+        'contact_email',
+        'logo_path',
+        'website'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'logo_path'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'password' => 'hashed'
+    ];
+
+    public function listings()
+    {
+        return $this->morphMany(Listing::class, 'listable');
+    }
+}
---
+++ database/migrations/2024_04_20_171520_create_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('password');
+            $table->string('location');
+            $table->string('email')->unique();
+            $table->string('contact_email');
+            $table->string('logo_path')->nullable();
+            $table->string('website')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
