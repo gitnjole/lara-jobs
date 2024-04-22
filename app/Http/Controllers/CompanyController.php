63d8bdd53d913304b835a9adc6b6a5bee72a2178
Antonio Cahun <cahunantonio1@gmail.com>
2024-04-22 21:26:36 +0200

    refactr(CompanyController): update controller to reflect new login method and resource creation

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -10,31 +10,32 @@ class CompanyController extends Controller
     /**
      * Method for storing new user in database
      *
-     * @param Request $request
+     * @param array $data
      *
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request): \Illuminate\Http\RedirectResponse
+    public function store(array $data): \Illuminate\Http\RedirectResponse
     {
-        if ($request->hasFile('logo')) {
-            $formFields['logo_path'] = $request->file('logo')->store('logos', 'public');
-        }
+        if ($data['logo'] instanceof UploadedFile) {
+            $data['logo_path'] = $data['logo']->store('logos', 'public');
+            unset($data['logo']);
+        }    
 
-        $formFields['password'] = bcrypt($formFields['password']);
+        $data['password'] = bcrypt($data['password']);
 
         try {
-            $company = Company::create($formFields);
+            $company = Company::create($data);
         } catch (\Exception $e) {
             dd($e->getMessage());
         }
 
         auth()->login($company);
         
-        return redirect('/')->with('message', 'Account created successfully! Welcome to LaraJobs.');
+        return redirect('/')->with('message', 'Company account created successfully! Welcome to LaraJobs.');
     }
     
     /**
-     * Method for updating user
+     * Method for updating company
      *
      * @param Request $request
      * @param Company $company
@@ -58,59 +59,6 @@ class CompanyController extends Controller
         $company->update($formFields);
         
         return redirect('/manage')->with('message', 'Account updated successfully!');
-    }
-    
-    /**
-     * Method for logging out user
-     *
-     * @param Request $request
-     *
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function logout(Request $request): \Illuminate\Http\RedirectResponse
-    {
-        auth()->logout();
-
-        $request->session()->invalidate();
-        $request->session()->regenerate();
-
-        return redirect('/')->with('message','You have logged out.');
-    }
-    
-    /**
-     * Method for showing login
-     * view
-     *
-     * @return \Illuminate\View\View
-     */
-    public function login(): \Illuminate\View\View
-    {
-        return view('users/login');
-    }
-    
-    /**
-     * Method for authenticating user login
-     *
-     * @param Request $request
-     *
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function authenticate(Request $request): \Illuminate\Http\RedirectResponse
-    {
-        $formFields = $request->validate([
-            'email' => "required|email|",
-            'password' => 'required',
-        ]);
-
-
-        if(auth()->attempt($formFields)) {
-            $request->session()->regenerate();
-
-            return redirect('/')->with('message','You are now logged in!');
-        }
-
-        return back()->withErrors(['email' => 'Invalid credentials'])->onlyInput('email');
-
     }
 
     public function profile()
