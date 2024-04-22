5f99cf9b4a9e2977c796956e43ffb508227f6771
Antonio Cahun <cahunantonio1@gmail.com>
2024-04-22 18:50:23 +0200

    feat(RegistrationController): implement registration specififc controller for handling web sign-ups

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistrationRequest;
+use Illuminate\Http\Request;
+
+class RegistrationController extends Controller
+{
+    public function create(): \Illuminate\View\View
+    {
+        return view('users/register');
+    }
+
+    public function store(RegistrationRequest $request)
+    {
+        if ($request['type'] === 'user') {
+            return app(UserController::class)->store($request->validatedData());
+        } else if ($request['type'] === 'company') {
+            return app(CompanyController::class)->store($request->validatedData());
+        } else {
+            dd('Error message');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\RegistrationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ListingController;
 
 /*
@@ -23,15 +25,36 @@ Route::get('/manage', [ListingController::class, 'manage'])->middleware('auth');
 
 /*
 |--------------------------------------------------------------------------
+| Registration routes
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::get('register', [RegistrationController::class, 'create']);
+Route::post('register', [RegistrationController::class, 'store']);
+
+/*
+|--------------------------------------------------------------------------
 | User routes
 |--------------------------------------------------------------------------
 |
 */
 
-Route::get('register', [UserController::class, 'create'])->middleware('guest');
-Route::post('register', [UserController::class, 'store'])->middleware('guest');
 Route::post('logout', [UserController::class, 'logout'])->middleware('auth');
 Route::get('login', [UserController::class, 'login'])->name('login')->middleware('guest');
 Route::post('users/authenticate', [UserController::class, 'authenticate'])->middleware('guest');
 Route::put('users/{user}', [UserController::class, 'put'])->middleware('auth');
 Route::get('{user}/profile', [UserController::class, 'profile'])->middleware('auth');
+
+/*
+|--------------------------------------------------------------------------
+| Company routes
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::post('logout', [CompanyController::class, 'logout'])->middleware('auth');
+Route::get('login', [CompanyController::class, 'login'])->name('login')->middleware('guest');
+Route::post('companys/authenticate', [CompanyController::class, 'authenticate'])->middleware('guest');
+Route::put('companys/{Company}', [CompanyController::class, 'put'])->middleware('auth');
+Route::get('{company}/profile', [CompanyController::class, 'profile'])->middleware('auth');
