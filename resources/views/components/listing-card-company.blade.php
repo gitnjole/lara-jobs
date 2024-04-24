ee5cc3053d45c72c7eabd047e04fea2a0d7ed6d3
Antonio Cahun <cahunantonio1@gmail.com>
2024-04-24 16:08:24 +0200

    feat(listing-card): split listing card into separate components

---
+++ resources/views/components/listing-card-company.blade.php
@@ -0,0 +1,21 @@
+@props(['listing'])
+
+<x-card>
+    <div class="flex">
+        <img
+            class="hidden w-48 mr-6 md:block"
+            src="{{ asset($listing->user?->pfp_path ? 'storage/' . $listing->user?->pfp_path : 'images/no-image.png') }}" 
+            alt=""
+        />
+        <div>
+            <h3 class="text-2xl">
+                <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
+            </h3>
+            <div class="text-xl font-bold mb-4">{{$listing->company['company_name']}}</div>
+            <x-listing-tags :tagsCsv="$listing['tags']" />
+            <div class="text-lg mt-4">
+                <i class="fa-solid fa-location-dot"></i> {{$listing->company['location']}}
+            </div>
+        </div>
+    </div>
+</x-card>
--- resources/views/components/listing-card-user.blade.php
+++ resources/views/components/listing-card-user.blade.php
@@ -4,14 +4,14 @@
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
-            src="{{ asset($listing->company->logo_path ? 'storage/' . $listing->company->logo_path : 'images/no-image.png') }}" class="logo"
+            src="{{ asset($listing->user?->pfp_path ? 'storage/' . $listing->user?->pfp_path : 'images/no-image.png') }}" 
             alt=""
         />
         <div>
             <h3 class="text-2xl">
                 <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
             </h3>
-            <div class="text-xl font-bold mb-4">{{$listing->user['company_name']}}</div>
+            <div class="text-xl font-bold mb-4">{{$listing->user['name']}}</div>
             <x-listing-tags :tagsCsv="$listing['tags']" />
             <div class="text-lg mt-4">
                 <i class="fa-solid fa-location-dot"></i> {{$listing->user['location']}}
